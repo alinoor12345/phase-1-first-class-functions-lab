9e37e9f4145913d86e9c43dfb49840d2ac023981
Ali Omar <ali.noor.student.moringaschool.com git config --gobal Ali abdullahigit config --global user.email ali.noor.student.moringaschool.com>
2022-12-05 12:10:00 +0300

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(array){
+    return array.slice(0,2);
+}
+
+
+//(-2)selects final two elements in an array
+const returnLastTwoDrivers = function(array){
+    return array.slice(-2)
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+let createFareMultiplier = function(int){
+    return function(value){
+        return (int * value)
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(drivers, choseDrivers){
+    return choseDrivers(drivers)
+}
